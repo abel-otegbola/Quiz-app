376e46fb6e901fc9f911a8cb98f17b0a28d5b446
abel-otegbola <abel15655@gmail.com>
2022-08-17 01:46:22 +0100

    Feat: Add category feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,56 @@
 //set variables to communicate with the DOM
 let pos = 0, test, category, difficulty, options, testStatus, button, choices, choice, correct, score = 0;
+
+            //DOM elements to be manipulated
             testStatus = document.querySelector("#test_status");
-            category = "science";
             difficulty = "easy";
             question = document.querySelector("#question");
             options = document.querySelectorAll("aside #optionbar label");
             button = document.querySelector("#test #optionbar button");
             scoreBoard = document.querySelector("aside #scoreboard");
 
-let apiAddress = `https://the-trivia-api.com/api/questions?categories=${category}&limit=10&difficulty=${difficulty}`;
-let loading = false;
+
+
+
+//Api address
 let questionsArray = []
 
 
-//Get array containing questions,options and answer from the api
-async function getQuestions() {
+//Get category selected by the user
+function getCategory() {
+    let allCategories = document.querySelectorAll(".category span");
+    for(let i=0; i<allCategories.length; i++) {
+        allCategories[i].addEventListener("click", () => {
+            for(let j=0; j<allCategories.length; j++) {
+                allCategories[j].classList.remove("active")
+            }
+            allCategories[i].classList.add("active")
+            category = allCategories[i].getAttribute("data-filter")
+            pos = 0
+            getQuestions(category)
+            score = 0;
+        })
+        if(allCategories[i].classList.contains("active")) {
+            category = allCategories[i].getAttribute("data-filter")
+            getQuestions()
+        }
+    }
+}
+getCategory()
 
-    loading = true;
+//Get array containing questions,options and answer from the api
+async function getQuestions(category) {
+
+    let apiAddress = `https://the-trivia-api.com/api/questions?categories=${category}&limit=10&difficulty=${difficulty}`;
+
+    let loading = document.querySelector(".loading");
+    loading.style.display = "flex"
 
     await fetch(apiAddress)
     .then(res => res.json())
     .then(data => {
         questionsArray = data;
-        loading = false;
+        loading.style.display = "none"
         loadQuestion()
     })
     .catch(err => {
@@ -32,7 +60,8 @@ async function getQuestions() {
     })
 
 }
-//Get initial questions from api
+
+//Load initial questions from api
 window.addEventListener("load", getQuestions(), false)
 
 
@@ -52,8 +81,6 @@ function loadQuestion() {
     //save answer into a variable
     correct = questionsArray[pos].correctAnswer;
 
-    console.log(optionsArray)
-
     for(let i=0; i<options.length; i++) {
         options[i].innerHTML = `<input type="radio" name="option" value="${optionsArray[i]}" class="options" id="option${i}" /> ${optionsArray[i]}`
     }
@@ -67,24 +94,23 @@ function loadQuestion() {
 
                 //check if the option checked is equal to the answer
                 if(choices[i].value === correct) {
-                    console.log(correct, "correct")
                     score++;
                 } else {
                     console.log(choices[i].value, "incorrect")
                 }
              }
                
-         }  //go to next question
+         }  
+         
+        //go to next question
         pos++;
-        setTimeout(function() {
-            //delays the next question to show the color change to darkgreen or darkred
-            if(pos === 10) {
-                window.alert(score)
-            }
-            else {
-                loadQuestion()
-            }
-        }, 300);   
+        //delays the next question to show the color change to darkgreen or darkred
+        if(pos > 9) {
+            window.alert(score)
+        }
+        else {
+            loadQuestion()
+        } 
         
      }
      //retrieves the players and scores upon loading of page
